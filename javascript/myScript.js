d956ea5cf6a11c6c248670b68f0e544ccc33d309
ldbhoang <ldbhoang@yahoo.com.vn>
2016-04-27 16:33:57 +0700

    Complete Json + AngularJS

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -1,11 +1,37 @@
 var myApp = angular.module("myCV", []);
 
-myApp.controller("header", function($scope) {
-    $scope.title= "Bé tập tính";
-});
-
 myApp.controller("getAboutMe", ['$scope', '$http', function($scope, $http){
 	$http.get('json/aboutMe.json').success(function(data) {
+		$scope.result = data;
+	});
+}]);
+
+myApp.controller("getSummary", ['$scope', '$http', function($scope, $http){
+	$http.get('json/summary.json').success(function(data) {
+		$scope.result = data;
+	});
+}]);
+
+myApp.controller("getExperience", ['$scope', '$http', function($scope, $http){
+	$http.get('json/experience.json').success(function(data) {
+		$scope.result = data;
+	});
+}]);
+
+myApp.controller("getProject", ['$scope', '$http', function($scope, $http){
+	$http.get('json/project.json').success(function(data) {
+		$scope.result = data;
+	});
+}]);
+
+myApp.controller("getSkill", ['$scope', '$http', function($scope, $http){
+	$http.get('json/skill.json').success(function(data) {
+		$scope.result = data;
+	});
+}]);
+
+myApp.controller("getEducation", ['$scope', '$http', function($scope, $http){
+	$http.get('json/education.json').success(function(data) {
 		$scope.result = data;
 	});
 }]);
